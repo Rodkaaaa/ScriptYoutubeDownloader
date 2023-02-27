874c23d7250c202f89d7667c0c9aea1433210582
Frederick Cid <88461868+Rodkaaaa@users.noreply.github.com>
2023-02-27 19:37:14 -0300

    Push

---
+++ main.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+
+SAVE_PATH =' C:/Users/Rodka/Desktop/Programacion/py/Youtue'
+
+link = input("Ingrese el link:")
+
+yt = YouTube(link)
+
+ys = yt.streams.get_highest_resolution()
+""" ys = yt.streams.get_audio_only("mp3") """
+
+print("downloading...")
+ys.download()
+print("downloaded")
